07a71a1cc74d5c3a16242cf852a5a85b6d550dde
Luka Rekhviashvili <rekhviashvili.luka125@gmail.com>
2022-06-09 12:19:50 +0400

    1. Added animation on first search. 2. Used API so that background changes according to the location.

--- main.js
+++ main.js
@@ -5,6 +5,13 @@ const api = {
     url: "https://api.openweathermap.org/data/2.5/"
 };
 
+const image_api = {
+    // curl https://api.unsplash.com/search/photos?query=wanderlust █
+    ac_key: "iqDmIgbiXdPcblljioVR3nX2Ue6KucibbYhl1rZ_BV4",
+    url: "https://api.unsplash.com/search/photos"
+}
+
+const body = document.querySelector("body");
 const main = document.querySelector(".main");
 const search = document.querySelector("#search-box");
 search.addEventListener('keypress', setQuery);
@@ -29,10 +36,25 @@ function createElements()
     const temp = document.createElement('div');
     const weatherType = document.createElement('div');
 
+    let height = 20;
+    let width = 400;
+    let i=1;
+    let myInterval = setInterval(()=>{
+        height++;
+        width+=10;
+        main.style.minHeight = height.toString() + "vh";
+        main.style.maxWidth = width.toString() + "px";  
+        i++;
+        if(i==30)
+        {
+            clearInterval(myInterval);
+        }
+    },5);
+
     location.classList.add("location");    
     temp.classList.add("temp");    
     weatherType.classList.add("weather-type");    
-    
+
     main.appendChild(location);
     main.appendChild(temp);
     main.appendChild(weatherType);
@@ -42,18 +64,37 @@ function createElements()
 function setResults(str)
 {
     fetch(`${api.url}weather?q=${str}&units=metric&APPID=${api.key}`).then(weather => {
+        //console.log(`${image_api.url}?query=${weather.json().name}&cliend_id=${image_api.ac_key}`);
+        /*fetch(`${image_api.url}?query=${weather.json().name}&cliend_id=${image_api.ac_key}`)
+        .then(response => {
+            return response.json();
+        }).then(changeBackground);*/
         return weather.json();
     }).then(displayResults);
+
 }
 
 function displayResults(weather)
 {
-    console.log(weather);
+    //console.log(weather);
     let location = document.querySelector('.location');
     let temp = document.querySelector('.temp');
     let realTemp = parseInt(weather.main.temp);
-    let weatherType = document.querySelector('.weather-type');
+    let weatherType = document.querySelector('.weather-type'); 
     location.innerText = `${weather.name}, ${weather.sys.country}`;
     temp.innerText = `${realTemp}°C`;
     weatherType.innerText = `${weather.weather[0].main}`;
+    changeBackground(weather);
+}
+
+function changeBackground(weather)
+{
+    fetch(`${image_api.url}?query=${weather.name}&client_id=${image_api.ac_key}`)
+        .then(response => {
+            return response.json();
+        })
+            .then((response) => {
+                console.log(response);
+                body.style.backgroundImage = `url(${response.results[response.results.length-1].urls.full})`;
+            });
 }
